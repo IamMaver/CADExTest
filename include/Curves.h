14da4957ff8495db3956197b0cdab8c0e0d681ec
forellik <forellik@student.21-school.ru>
2024-10-23 14:34:32 +0300

    Check for positive radius added

--- include/Curves.h
+++ include/Curves.h
@@ -5,13 +5,18 @@
 #ifndef CADEXTEST_CURVES_H
 #define CADEXTEST_CURVES_H
 
+#include <stdexcept>
 #include "Shape.h"
 
 class Circle : public Shape {
 private:
     double _radius;
 public:
-    Circle(double radius, Point center = {0, 0, 0}) : Shape(center), _radius(radius) {}
+    Circle(double radius, Point center = {0, 0, 0}) : Shape(center), _radius(radius) {
+        if (radius <= 0) {
+            throw std::invalid_argument("Radius must be positive.");
+        }
+    }
 
     Point getPoint(double t) const override;
 
@@ -28,7 +33,11 @@ private:
     double _radiusY;
 public:
     Ellipse(double radiusX, double radiusY, Point center = {0, 0, 0})
-            : Shape(center), _radiusX(radiusX), _radiusY(radiusY) {}
+            : Shape(center), _radiusX(radiusX), _radiusY(radiusY) {
+        if (radiusX <= 0 || radiusY <= 0) {
+            throw std::invalid_argument("Radius must be positive.");
+        }
+    }
 
     Point getPoint(double t) const override;
 
@@ -41,7 +50,11 @@ private:
     double _step;
 public:
     Helix(double radius, double step, Point center = {0, 0, 0})
-            : Shape(center), _radius(radius), _step(step) {}
+            : Shape(center), _radius(radius), _step(step) {
+        if (radius <= 0) {
+            throw std::invalid_argument("Radius must be positive.");
+        }
+    }
 
     Point getPoint(double t) const override;
 
